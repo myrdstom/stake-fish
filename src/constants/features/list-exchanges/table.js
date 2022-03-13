7749fa59854c8b7168bcb3b7a9c3c7d6da74d534
myrdstom <nsereko.paul@agilis-partners.com>
2022-03-13 14:54:20 +0300

    Update exchange table to more clearly show the exchange details button

--- src/constants/features/list-exchanges/table.js
+++ src/constants/features/list-exchanges/table.js
@@ -4,5 +4,5 @@ export const columns = [
   { id: "country", label: "Country", minWidth: 170 },
   { id: "url", label: "Company Website", minWidth: 170 },
   { id: "trust rank", label: "Rank", minWidth: 170 },
-  { id: "action", label: "Action", minWidth: 170 }
+  { id: "action", label: "Details", minWidth: 170 }
 ];
